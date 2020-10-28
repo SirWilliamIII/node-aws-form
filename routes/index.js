c7b16cbf3f9047d9a119a9f0b34adfc6f99ca880
SirWilliamIII <willcarpenterwastaken@gmail.com>
2020-10-27 23:22:37 -0500

    node form

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express'
+    });
 });
+
+const bodyParser = require('body-parser')
+
+
+router.post('/submit', (req, res) => {
+    console.log('Username: ' + req.body.username)
+    console.log('Password: ' + req.body.password)
+    res.redirect('/')
+})
+
+
+
 
 module.exports = router;
